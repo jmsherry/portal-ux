0af9405a1cc976fc23c3140226bc432384fed6c6
James Sherry <james.m.sherry@googlemail.com>
2023-02-14 13:28:53 +0000

    color overrides on list

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -15,7 +15,7 @@ function CoursePage() {
       <h1 className="text-3xl font-bold mb-4">{course.timescale} {course.title} ({course.tuition})</h1>
       <ul className="mx-auto divide-y divide-gray-200 dark:divide-gray-700 grid grid-cols-12">
           {course.lessons.map(({ _id, title, completed, unit }) => (
-            <li key={_id} className={`p-3 sm:p-4 text-left ${completed ? 'bg-neutral-400' : ''} ${colorMap.get(unit)} flex items-center border-black border aspect-square`}>
+            <li key={_id} className={`p-3 sm:p-4 text-left ${completed ? 'bg-neutral-400' : colorMap.get(unit) } flex items-center border-black border aspect-square`}>
               <Link to={`/lesson/${title}`} className={unit === 'JS' ? 'text-black' : "text-white"}>{title}</Link>
             </li>
           ))}
